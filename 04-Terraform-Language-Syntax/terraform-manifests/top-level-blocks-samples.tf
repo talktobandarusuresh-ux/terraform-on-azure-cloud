6601506f634eac7ef8acc460fd0a9e1f8e928551
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-02-06 16:43:59 +0530

    Terraform

--- 04-Terraform-Language-Syntax/terraform-manifests/top-level-blocks-samples.tf
+++ 04-Terraform-Language-Syntax/terraform-manifests/top-level-blocks-samples.tf
@@ -11,7 +11,7 @@ terraform {
   # Terraform State Storage to Azure Storage Container
   backend "azurerm" {
     resource_group_name  = "terraform-storage-rg"
-    storage_account_name = "terraformstate201"
+    storage_account_name = "terraformstate201111"
     container_name       = "tfstatefiles"
     key                  = "terraform.tfstate"
   }
