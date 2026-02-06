d5385318cdc43a95b0250bbfeaf5c220b20adba1
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-02-06 14:11:43 +0530

    Terraform

--- git-deploy.sh
+++ git-deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Add files and do local commit"
 git add .
-git commit -am "Welcome to StackSimplify"
+git commit -am "Welcome to Azure Cloud"
 
 echo "Pushing to Github Repository"
 git push
